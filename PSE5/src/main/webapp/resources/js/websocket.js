8c5d9dbb40580535898fea7fd827cb3684f988be
Pablo Martínez <bribi@DESKTOP-RGRQBFM>
2018-05-13 14:25:49 +0200

    Chat working!

--- PSE5/src/main/webapp/resources/js/websocket.js
+++ PSE5/src/main/webapp/resources/js/websocket.js
@@ -1,19 +1,27 @@
+//To disable on keypress actions with primefaces
+$('form').off('keypress.disableAutoSubmitOnEnter').on('keypress.disableAutoSubmitOnEnter', function(event) {
+    if (event.which === $.ui.keyCode.ENTER && $(event.target).is(':input:not(textarea,:button,:submit,:reset)')) {
+        event.preventDefault();
+    }
+});
+
 var wsUri = 'ws://' + document.location.host
         + document.location.pathname.substr(0,
                 document.location.pathname.indexOf("/faces")) + '/websocket';
 console.log(wsUri);
-var websocket = new WebSocket(wsUri); //Inicializa el websocket
+var websocket = new WebSocket(wsUri);//Inicializa el websocket
 var textField = document.getElementById("texto");
 var users = document.getElementById("users");
 var chatlog = document.getElementById("chatlog");
 var output = document.getElementById("output");
-var username;
+var username =textField.value;
 function join() {
-    username = textField.value;
     websocket.send(username + " joined");
+    $("#texto").val('');
     document.getElementById("unirse").style.setProperty("visibility", "hidden");
     document.getElementById("enviar").style.removeProperty("visibility");
     document.getElementById("desconectar").style.removeProperty("visibility");
+    document.getElementById("texto").style.removeProperty("visibility");
 }
 function send_message() {
     websocket.send(username + ": " + textField.value);
@@ -23,12 +31,13 @@ function disconnect() {
     document.getElementById("unirse").style.setProperty("visibility", "hidden");
     document.getElementById("enviar").style.setProperty("visibility", "hidden");
     document.getElementById("desconectar").style.setProperty("visibility", "hidden");
+    window.location.href = "http://localhost:8080/PSE5/faces/index.xhtml";
 }
-websocket.onopen = function (evt) {
-    writeToScreen("CONNECTED");
+websocket.onopen = function () {
+    writeToScreen("WEB SOCKET CONNECTION ESTABLISHED");
 };
-websocket.onclose = function (evt) {
-    writeToScreen("DISCONNECTED");
+websocket.onclose = function () {
+    writeToScreen("WEB SOCKET CONNECTION CLOSED");
 };
 websocket.onmessage = function (evt) {
     writeToScreen("RECEIVED: " + evt.data);
@@ -42,10 +51,10 @@ websocket.onerror = function (evt) {
     writeToScreen('<span style="color: red;">ERROR:</span> ' + evt.data);
 };
 function writeToScreen(message) {
+    //Muestro por pantalla un mensaje con la situación del websocket
+    output.innerHTML = "";
     var pre = document.createElement("p");
     pre.style.wordWrap = "break-word";
     pre.innerHTML = message;
     output.appendChild(pre);
 }
-
-
